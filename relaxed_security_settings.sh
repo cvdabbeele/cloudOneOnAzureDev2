032bbc10fadb1e632cae961ce693e40ad54d432f
cvdabbeele <cvdabbeele@gmail.com>
2021-02-08 09:46:20 +0100

    Added scripts to change the security settings

--- relaxed_security_settings.sh
+++ relaxed_security_settings.sh
@@ -1,6 +1,6 @@
 STARTDIR=`pwd`
 
 cd ~/apps/c1-app-sec-moneyx
-sed -i 's/": 300/": 0/g' azure-pipelines.yml
+sed -i 's/": 0/": 300/g' azure-pipelines.yml
  echo " "  >>README.md  && git add . && git commit -m "relaxed security settings"  && git push  --set-upstream origin master
 cd $STARTDIR
